e8d5e8869fc4b35b08b62d8b9db68a7fd76bbaae
Indra <radeonaru@gmail.com>
2024-02-28 18:46:19 +0700

    feat: add table category beauty_health on views

--- resources/views/blog/beauty_health.blade.php
+++ resources/views/blog/beauty_health.blade.php
@@ -1,3 +1,48 @@
 <html>
-    
+    <head>
+        <title>Product Sales</title>
+        <style>
+            table {
+                width: 100%;
+                border-collapse: collapse;
+            }
+            th, td {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+            th {
+                background-color: #f2f2f2;
+            }
+        </style>
+        </head>
+        <body>
+    <h2>Product Sales for Beauty Health:</h2>
+<table>
+    <tr>
+        <th>Product Name</th>
+        <th>Price per Unit (IDR)</th>
+    </tr>
+    <tr>
+        <td>Facial Cream</td>
+        <td>200,000</td>
+    </tr>
+    <tr>
+        <td>Shampoo</td>
+        <td>120,000</td>
+    </tr>
+    <tr>
+        <td>Hand Cream</td>
+        <td>80,000</td>
+    </tr>
+    <tr>
+        <td>Lipstick</td>
+        <td>150,000</td>
+    </tr>
+    <tr>
+        <td>Face Masks</td>
+        <td>50,000</td>
+    </tr>
+</table>
+</body>
 </html>
