1cbb751b9c4cc3df6c5ab9270b2a335e1e20920e
Indra <radeonaru@gmail.com>
2024-02-28 18:52:46 +0700

    feat: add table category home_care on views

--- resources/views/blog/home_care.blade.php
+++ resources/views/blog/home_care.blade.php
@@ -1,3 +1,48 @@
 <html>
-    
+    <head>
+        <title>Product Sales</title>
+        <style>
+            table {
+                width: 100%;
+                border-collapse: collapse;
+            }
+            th, td {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+            th {
+                background-color: #f2f2f2;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>Product Sales for Home Care:</h2>
+<table>
+    <tr>
+        <th>Product Name</th>
+        <th>Price per Unit (IDR)</th>
+    </tr>
+    <tr>
+        <td>Laundry Detergent</td>
+        <td>100,000</td>
+    </tr>
+    <tr>
+        <td>Dish Soap</td>
+        <td>50,000</td>
+    </tr>
+    <tr>
+        <td>All-Purpose Cleaner</td>
+        <td>80,000</td>
+    </tr>
+    <tr>
+        <td>Air Freshener</td>
+        <td>40,000</td>
+    </tr>
+    <tr>
+        <td>Trash Bags</td>
+        <td>60,000</td>
+    </tr>
+</table>
+    </body>
 </html>
